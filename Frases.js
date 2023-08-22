edaa019b425fbd645efc09e5d6d24d034bcc6bc2
francoPaez25 <soypaez17@hotmail.com>
2023-08-22 10:51:02 -0300

    first commit

---
+++ Frases.js
@@ -0,0 +1,130 @@
+let citas = [
+    {
+      'autor': 'Albert Einstein',
+      'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+    },
+    {
+      'autor': 'Alan Kay',
+      'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+    },
+    {
+      'autor': 'Amelia Earhart',
+      'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+    },
+    {
+      'autor': 'Aristotle',
+      'texto': 'La calidad no es un acto, es un hábito.'
+    },
+    {
+      'autor': 'Benjamin Franklin',
+      'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+    },
+    {
+      'autor': 'Charles R. Swindoll',
+      'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+    },
+    {
+      'autor': 'Jane Goodall',
+      'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+    },
+    {
+      'autor': 'John Muir',
+      'texto': 'El poder de la imaginación nos hace infinitos.'
+    },
+    {
+      'autor': 'Steve Jobs',
+      'texto': 'El único modo de hacer un gran trabajo es amar lo que haces.'
+    },
+    {
+      'autor': ' Winston Churchill',
+      'texto': 'El éxito no es definitivo, el fracaso no es fatal: lo que cuenta es el valor para continuar..'
+    },
+    {
+      'autor': 'Charles R. Swindoll',
+      'texto': 'La vida es 10% lo que nos sucede y 90% cómo reaccionamos ante ello.'
+    },
+    {
+      'autor': 'Eleanor Roosevelt',
+      'texto': 'El futuro pertenece a quienes creen en la belleza de sus sueños.'
+    },
+    {
+      'autor': ' Amelia  Earhart',
+      'texto': 'Nuestros mayores miedos están en nuestra mente. Conquístalos y encontrarás que siempre han sido solo nubes.'
+    },
+    {
+      'autor': ' Pablo Picasso',
+      'texto': 'La acción es la clave fundamental para todo éxito.'
+    },
+    {
+      'autor': ' Nelson Mandela',
+      'texto': 'Siempre parece imposible hasta que se hace.'
+    },
+    {
+      'autor': 'Confucio',
+      'texto': 'No importa lo lento que vayas, siempre y cuando no te detengas.'
+    },
+    {
+      'autor': 'Robert Collier',
+      'texto': 'El éxito es la suma de pequeños esfuerzos repetidos día tras día.'
+    },
+    {
+      'autor': 'Proverbio chino',
+      'texto': 'El fracaso no es caer, sino negarse a levantarse.'
+    },
+    {
+      'autor': 'Henry Ford',
+      'texto': 'No encuentres la falta, encuentra el remedio.'
+    },
+    {
+      'autor': 'Albert Einstein',
+      'texto': 'La vida es como andar en bicicleta. Para mantener el equilibrio, debes seguir adelante.'
+    },
+    {
+      'autor': 'Brian Tracy',
+      'texto': 'No importa quién eres, ni de dónde vienes. Lo único que importa es hacia dónde te diriges.'
+    },
+    {
+      'autor': 'Michael Jordan',
+      'texto': 'Nunca digas nunca, porque los límites son como miedos: a menudo son solo una ilusión.'
+    },
+    {
+      'autor': 'John Wooden',
+      'texto': 'No importa cuántas veces fracases, debes seguir intentándolo. No es la cantidad de veces que fallas lo que importa, sino la cantidad de veces que lo intentas de nuevo.'
+    },
+    {
+      'autor': 'Will Rogers',
+      'texto': 'No dejes que ayer ocupe demasiado de hoy.'
+    },
+    {
+      'autor': ' Ralph Waldo Emerson',
+      'texto': 'La confianza en sí mismo es el primer paso para el éxito.'
+    },
+    {
+      'autor': 'Confucio',
+      'texto': 'La mayor gloria no es nunca caer, sino levantarse cada vez que caemos.'
+    },
+    {
+      'autor': 'Muhammad Ali',
+      'texto': 'No cuentes los días, haz que los días cuenten.'
+    },
+    {
+      'autor': 'George Eliot',
+      'texto': 'Nunca es demasiado tarde para ser lo que podrías haber sido.'
+    },
+    {
+      'autor': 'Henry Ford',
+      'texto': 'El fracaso es la oportunidad de comenzar de nuevo, pero con más inteligencia.'
+    },
+    {
+      'autor': 'Jim Rohn',
+      'texto': 'La disciplina es el puente entre metas y logros.'
+    },
+    {
+      'autor': 'Mark Twain',
+      'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+    },   
+    {
+      'autor': 'Anonimo',
+      'texto': 'Con un pequeño paso al día estas avanzando hacia tu objetivo.'
+    }   
+  ];
